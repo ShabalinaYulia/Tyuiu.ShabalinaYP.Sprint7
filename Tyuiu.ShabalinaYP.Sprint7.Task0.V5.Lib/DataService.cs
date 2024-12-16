a5463e280075bac5407448e60e2196a5f8651935
ShabalinaYulia <julia07.kurash@gmail.com>
2024-12-16 22:18:12 +0500

    Add project files.

---
+++ Tyuiu.ShabalinaYP.Sprint7.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+namespace Tyuiu.ShabalinaYP.Sprint7.Task0.V5.Lib
+{
+    public class DataService
+    {
+        public List<string[]> WholesaleBase(string path)
+        {
+            List<string[]> items = new List<string[]>();
+            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    items.Add(line.Split(';'));
+                }
+            }
+            return items;
+        }
+    }
+}
---
+++ Tyuiu.ShabalinaYP.Sprint7.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ShabalinaYP.Sprint7.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
